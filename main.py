400202753829f79b1793c6e52dec7905fac9427c
bhaktidn0311 <bhaktidn0321@gmail.com>
2026-01-22 14:53:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
-print("Hello, my name is Vaishnavi")
+print("Hello, my name is Bhakti")
 print("I am a BTech student in Artificial Intelligence & Data Science")
 print("This is my first project using Git and GitHub")
